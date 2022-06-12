5441944469de8bb78c4fab83bd9aac5e38fcd749
noahglad <ayanboden@gmail.comm>
2022-06-12 21:12:02 +0100

    just testing

---
+++ main.py
@@ -0,0 +1,14 @@
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self):
+        pass
+
+
+
+Ade = Student(name="Ade", age=24, tracks=["FS","BE"],score=22.80)
+
+# Expected methods
+Ade.change_name("Peter")
+Ade.change_age(34)
+Ade.add_track("UI/UX")
+Ade.get_score()
